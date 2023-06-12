0543232c077fe39efc2c8b234e5ce32abcdada64
Galahad <gabrielbbaldez@outlook.com>
2023-06-12 18:09:07 -0300

    Commit inicial
    Criação das propriedades da api usando spring initializr

---
+++ api/src/main/java/klab/DeckOfCards/api/DeckOfCardsApplication.java
@@ -0,0 +1,13 @@
+package klab.DeckOfCards.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckOfCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckOfCardsApplication.class, args);
+	}
+
+}
