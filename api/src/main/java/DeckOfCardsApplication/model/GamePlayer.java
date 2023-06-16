073c8053736b9d415343b705ae166353cd67c5ac
Galahad <gabrielbbaldez@outlook.com>
2023-06-16 16:20:27 -0300

    Ajuste Tabelas GamePlayer e Winner
    - Trocado tipos de dados numéricos de Long para Integer

--- api/src/main/java/DeckOfCardsApplication/model/GamePlayer.java
+++ api/src/main/java/DeckOfCardsApplication/model/GamePlayer.java
@@ -14,7 +14,7 @@ public class GamePlayer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "game_id")
