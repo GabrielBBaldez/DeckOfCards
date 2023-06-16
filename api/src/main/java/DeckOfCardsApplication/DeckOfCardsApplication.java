f4ce3603614f2342baa926316937e918ea23265c
Galahad <gabrielbbaldez@outlook.com>
2023-06-15 23:36:05 -0300

    Update DeckOfCardsApplication.java

--- api/src/main/java/DeckOfCardsApplication/DeckOfCardsApplication.java
+++ api/src/main/java/DeckOfCardsApplication/DeckOfCardsApplication.java
@@ -1,33 +1,13 @@
 package DeckOfCardsApplication;
 
-import DeckOfCardsApplication.services.DeckOfCardsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import java.util.Scanner;
-
-import java.util.List;
 
 @SpringBootApplication
-@EnableFeignClients
+
 public class DeckOfCardsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeckOfCardsApplication.class, args);
-		DeckOfCardsService deckOfCardsService = new DeckOfCardsService();
-		Scanner scanner = new Scanner(System.in);
-		boolean jogarNovamente = true;
-
-		while (jogarNovamente) {
-			deckOfCardsService.playDeckOfCards();
-
-			System.out.print("Deseja jogar novamente? (S/N): ");
-			String resposta = scanner.nextLine();
-
-			if (resposta.equalsIgnoreCase("N")) {
-				jogarNovamente = false;
-			}
-		}
-		System.out.println("Obrigado por jogar!");
 	}
 }
